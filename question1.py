7d1069e409c8775813513f1e4e1cc037ec8a9026
Vadim <vadim.marakushin@gmail.com>
2025-09-22 12:00:42 +0300

    Initial commit

---
+++ question1.py
@@ -0,0 +1,86 @@
+from datetime import timedelta
+
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import String, Float, Integer, ForeignKey, DateTime, Interval
+
+
+class Base(DeclarativeBase):
+	pass
+
+
+class GenreBase(Base):
+	__tablename__ = "genre"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	genre_name: Mapped[str] = mapped_column(String(50))
+
+
+class BookBase(Base):
+	__tablename__ = "book"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	book_name: Mapped[str] = mapped_column(String(50))
+	price: Mapped[float] = mapped_column(Float)
+	count: Mapped[int] = mapped_column(Integer)
+	genre_id: Mapped[int] = mapped_column(ForeignKey("GenreBase.id"))
+	author_id: Mapped[int] = mapped_column(ForeignKey("AuthorBase.id"))
+
+
+class AuthorBase(Base):
+	__tablename__ = "author"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	author_name: Mapped[str] = mapped_column(String(50))
+
+
+class CityBase(Base):
+	__tablename__ = "city"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	city_name: Mapped[str] = mapped_column(String(50))
+	shipping_time: Mapped[timedelta] = mapped_column(Interval)
+
+
+class ClientBase(Base):
+	__tablename__ = "client"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	client_name: Mapped[str] = mapped_column(String(50))
+	client_email: Mapped[str] = mapped_column(String(50))
+	client_city: Mapped[int] = mapped_column(ForeignKey("CityBase.id"))
+
+
+class OrderBase(Base):
+	__tablename__ = "order"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	clients_wishes: Mapped[str] = mapped_column(String())
+	client_id: Mapped[int] = mapped_column(ForeignKey("ClientBase.id"))
+
+
+class OrderBookBase(Base):
+	__tablename__ = "order_book"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	count: Mapped[int] = mapped_column(Integer)
+	book_id: Mapped[int] = mapped_column(ForeignKey("BookBase.id"))
+	order_id: Mapped[int] = mapped_column(ForeignKey("OrderBase.id"))
+
+
+class StepBase(Base):
+	__tablename__ = "step"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	step_name: Mapped[str] = mapped_column(String(50))
+
+
+class OrderStepBase(Base):
+	__tablename__ = "order_step"
+
+	id: Mapped[int] = mapped_column(primary_key=True)
+	order_start: Mapped[DateTime] = mapped_column(DateTime)
+	order_end: Mapped[DateTime] = mapped_column(DateTime)
+	step_id: Mapped[int] = mapped_column(ForeignKey("StepBase.id"))
+	client_id: Mapped[int] = mapped_column(ForeignKey("ClientBase.id"))
---
+++ question2.py
@@ -0,0 +1,179 @@
+import re, requests, pandas as pd
+import time
+from dataclasses import dataclass
+from datetime import datetime
+from sqlalchemy import create_engine, text, String, Integer, DateTime, MetaData
+
+from bs4 import BeautifulSoup
+from sqlalchemy.orm import sessionmaker, DeclarativeBase, Mapped, mapped_column
+
+start_time = time.time()
+
+
+@dataclass(slots=True)
+class Trade:
+    exchange_product_id: str | None = None
+    exchange_product_name: str | None = None
+    oil_id: str | None = None
+    delivery_basis_id : str | None = None
+    delivery_basis_name: str | None = None
+    delivery_type_id: str | None = None
+    volume: int | None = None
+    total: float | None = None
+    count: int | None = None
+    date: datetime | None = None
+    created_on: datetime | None = datetime.now()
+    updated_on: datetime | None = datetime.now()
+
+
+class SpimexParser:
+    def __init__(self, path: str):
+        self.path = path
+        self.exel_file = pd.read_excel(self.path)
+        self.date = datetime.strptime(self.exel_file.iloc[2]['Форма СЭТ-БТ'].replace('Дата торгов: ', ''), '%d.%m.%Y')
+        self.trade_list = []
+
+    def parse(self):
+        exel_file = pd.read_excel(self.path)
+        row_number = int(exel_file[exel_file['Форма СЭТ-БТ'] == 'Единица измерения: Метрическая тонна'].index[0]) + 4
+        data_columns = ['pass1', 'exchange_product_id', 'exchange_product_name',
+                        'delivery_basis_name', 'volume', 'total', 'pass2'
+            , 'pass3', 'pass4', 'pass5', 'pass6', 'pass7', 'pass8', 'pass9', 'count']
+        columns_to_drop = ['pass1', 'pass2', 'pass3', 'pass4', 'pass5', 'pass6', 'pass7', 'pass8', 'pass9']
+        exel_file = pd.read_excel(self.path,
+                                  skiprows=row_number,
+                                  header=None,
+                                  names=data_columns
+                                  )
+        exel_file = exel_file.drop(columns=columns_to_drop, axis=1)
+        return exel_file
+
+    def read_data(self):
+        entries_list = self.parse()
+        ignore_words = ('Маклер СПбМТСБ','Маклер АО Петербургская Биржа','25/25','24/24','22/22','21/21','20/20','19/19','19/19','16/16','17/17','15/15','Итого:','Итого по секции:',)
+        for entry in range(0, len(entries_list)):
+            current_entry = entries_list.iloc[entry]
+            if current_entry['count'] != '-' and current_entry['exchange_product_id'] not in ignore_words:
+                trade = Trade(
+                    exchange_product_id=entries_list.iloc[entry]['exchange_product_id'],
+                    exchange_product_name=entries_list.iloc[entry]['exchange_product_name'],
+                    oil_id=entries_list.iloc[entry]['exchange_product_id'][:4],
+                    delivery_basis_id=entries_list.iloc[entry]['exchange_product_id'][4:7],
+                    delivery_basis_name=entries_list.iloc[entry]['delivery_basis_name'],
+                    delivery_type_id=entries_list.iloc[entry]['exchange_product_id'][-1],
+                    volume=int(entries_list.iloc[entry]['volume']),
+                    total=float(entries_list.iloc[entry]['total']),
+                    count=int(entries_list.iloc[entry]['count']),
+                    date=self.date
+                )
+                self.trade_list.append(trade)
+        return self.trade_list
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class SpimexTradingResultsBase(Base):
+    __tablename__ = "spimex_trading_results"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    exchange_product_id: Mapped[str] = mapped_column(String(20))
+    exchange_product_name: Mapped[str] = mapped_column(String())
+    oil_id: Mapped[str] = mapped_column(String(4))
+    delivery_basis_id : Mapped[str] = mapped_column(String(3))
+    delivery_basis_name: Mapped[str] = mapped_column(String(50))
+    delivery_type_id: Mapped[str] = mapped_column(String(1))
+    volume: Mapped[int] = mapped_column(Integer)
+    total: Mapped[int] = mapped_column(Integer)
+    count: Mapped[int] = mapped_column(Integer)
+    date: Mapped[datetime] = mapped_column(DateTime)
+    created_on: Mapped[datetime] = mapped_column(DateTime)
+    updated_on: Mapped[datetime] = mapped_column(DateTime)
+
+
+
+engine = create_engine('postgresql+psycopg2://postgres:Oblivion94$@localhost:5432/spimex_trading_results')
+metadata = MetaData()
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+PAGE_NUMBER = 1
+
+session_exceptions = 0
+
+while True:
+    URL = f"https://spimex.com/markets/oil_products/trades/results/?page=page-{PAGE_NUMBER}"
+    LINKS_PATTERN = re.compile(r'^/upload/reports/oil_xls/oil_xls_202([543]).*')
+    file_path = 'bulletin_file.xls'
+    response = requests.get(URL)
+    html = response.text
+    soup = BeautifulSoup(html, "html.parser")
+    links = soup.find_all('a', attrs={'class': 'accordeon-inner__item-title link xls',
+                                      'href': re.compile(LINKS_PATTERN)})
+
+
+    for i, link in enumerate(links):
+        LINK_XLS_URL = f"https://spimex.com{link['href']}"
+        print(f'Обрабатывается страница - {PAGE_NUMBER}, файл № - {i}')
+        try:
+            response = requests.get(LINK_XLS_URL)
+            response.raise_for_status()
+
+            with open(file_path, 'wb') as f:
+                f.write(response.content)
+
+        except requests.exceptions.RequestException as e:
+            session_exceptions += 1
+            print(f"Ошибка при скачивании файла: {e}")
+
+
+        try:
+            parser_spimex = SpimexParser(file_path)
+
+            parser_data = parser_spimex.read_data()
+
+        except Exception as e:
+            session_exceptions += 1
+            print(f"Ошибка при чтении файла: {e}")
+
+
+        try:
+            with engine.connect() as connection:
+                for obj in parser_data:
+                    record = SpimexTradingResultsBase(
+                        exchange_product_id=obj.exchange_product_id,
+                        exchange_product_name=obj.exchange_product_name,
+                        oil_id=obj.oil_id,
+                        delivery_basis_id=obj.delivery_basis_id,
+                        delivery_basis_name=obj.delivery_basis_name,
+                        delivery_type_id=obj.delivery_type_id,
+                        volume=obj.volume,
+                        total=obj.total,
+                        count=obj.count,
+                        date=obj.date,
+                        created_on=obj.created_on,
+                        updated_on=obj.updated_on
+                    )
+                    session.add(record)
+                session.commit()
+
+        except Exception as e:
+            session_exceptions += 1
+            print(f"Ошибка при записи в базу данных: {e}")
+
+    PAGE_NUMBER += 1
+
+    if parser_spimex.date == datetime.strptime('2023-01-09 00:00:00', '%Y-%d-%m %H:%M:%S'):
+        break
+
+end_time = time.time()
+duration = (end_time - start_time) / 60
+
+total_count = session.query(SpimexTradingResultsBase).count()
+print(f'Общее количество записей в базе данных - {total_count}')
+print(f'Общее время выполнения программы - {duration} минут')
+print(f'Общее количество ошибок за время выполнения программы - {session_exceptions}')
+
+
