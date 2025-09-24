4e94fdf8ad44da10bf04e8adab1e4c41ed6023c7
Vadim <vadim.marakushin@gmail.com>
2025-09-24 14:25:08 +0300

    reformat code

--- spimex_parser/db.py
+++ spimex_parser/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData
+from sqlalchemy import create_engine, MetaData, select, exists
 from sqlalchemy.orm import sessionmaker
 
 from spimex_parser.config import Config
@@ -12,29 +12,33 @@ class Database:
         Base.metadata.create_all(self.engine)
         self.Session = sessionmaker(bind=self.engine)
 
-    def add_records(self, records):
+    def add_records(self, records: list):
         with self.Session() as session:
             try:
-                records_list = [
-                    SpimexTradingResultsBase(
-                        exchange_product_id=obj.exchange_product_id,
-                        exchange_product_name=obj.exchange_product_name,
-                        oil_id=obj.oil_id,
-                        delivery_basis_id=obj.delivery_basis_id,
-                        delivery_basis_name=obj.delivery_basis_name,
-                        delivery_type_id=obj.delivery_type_id,
-                        volume=obj.volume,
-                        total=obj.total,
-                        count=obj.count,
-                        date=obj.date,
-                        created_on=obj.created_on,
-                        updated_on=obj.updated_on,
-                    )
-                    for obj in records
-                ]
-                session.add_all(records_list)
-                session.commit()
+                query = select(exists().where(
+                    SpimexTradingResultsBase.date == records[0].date
+                ))
+                if not session.scalar(query):
+                    records_list = [
+                        SpimexTradingResultsBase(
+                            exchange_product_id=obj.exchange_product_id,
+                            exchange_product_name=obj.exchange_product_name,
+                            oil_id=obj.oil_id,
+                            delivery_basis_id=obj.delivery_basis_id,
+                            delivery_basis_name=obj.delivery_basis_name,
+                            delivery_type_id=obj.delivery_type_id,
+                            volume=obj.volume,
+                            total=obj.total,
+                            count=obj.count,
+                            date=obj.date,
+                            created_on=obj.created_on,
+                            updated_on=obj.updated_on,
+                        )
+                        for obj in records
+                    ]
+                    session.add_all(records_list)
+                    session.commit()
             except Exception as e:
                 print(f"Ошибка при записи в БД: {e}")
                 session.rollback()
-                raise
+                raise e
--- spimex_parser/parser.py
+++ spimex_parser/parser.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import re
 from datetime import datetime
 
@@ -25,20 +26,34 @@ class SpimexXlsDownloader:
             response.raise_for_status()
             with open(self.file_path, "wb") as f:
                 f.write(response.content)
+            if not os.path.exists(self.file_path):
+                raise FileNotFoundError
             logging.info(f"Файл успешно скачан: {link_url}")
+
+
 
         except requests.exceptions.HTTPError as http_err:
             logging.error(f"HTTP ошибка при скачивании файла: {http_err}")
+            raise http_err
         except requests.exceptions.ConnectionError as conn_err:
             logging.error(f"Ошибка подключения: {conn_err}")
+            raise conn_err
         except requests.exceptions.Timeout as timeout_err:
             logging.error(f"Превышено время ожидания: {timeout_err}")
+            raise timeout_err
+        except FileNotFoundError as e:
+            logging.error(f'Файл не найден {e}')
+            raise e
+        except IndexError as e:
+            logging.error(f'Ссылки не найдены {e}')
+            raise e
         except Exception as e:
             logging.error(f"Неизвестная ошибка при скачивании файла: {e}")
+            raise e
 
 
 class SpimexWebParser:
-    def __init__(self, file_path: str, page_number: int = 67):
+    def __init__(self, file_path: str, page_number: int = 1):
         self.file_path = file_path
         self.exel_file = None
         self.date = None
@@ -72,14 +87,21 @@ class SpimexWebParser:
 
         except requests.exceptions.HTTPError as http_err:
             logging.error(f"HTTP ошибка при получении ссылок: {http_err}")
+            raise http_err
         except requests.exceptions.ConnectionError as conn_err:
             logging.error(f"Ошибка подключения при получении ссылок: {conn_err}")
+            raise conn_err
         except requests.exceptions.Timeout as timeout_err:
             logging.error(
                 f"Превышено время ожидания при получении ссылок: {timeout_err}"
             )
+            raise timeout_err
+        except IndexError as e:
+            logging.error(f'Ссылки не найдены {e}')
+            raise e
         except Exception as e:
             logging.error(f"Неизвестная ошибка при получении ссылок: {e}")
+            raise e
 
         self.page_number += 1
         self.url = f"https://spimex.com/markets/oil_products/trades/results/?page=page-{self.page_number}"
@@ -87,41 +109,47 @@ class SpimexWebParser:
 
     def parse(self):
         try:
-            exel_file = pd.read_excel(self.file_path)
-            self.date = datetime.strptime(
-                exel_file.iloc[2]["Форма СЭТ-БТ"].replace("Дата торгов: ", ""),
-                "%d.%m.%Y",
-            )
-            row_number = (
-                int(
-                    exel_file[
-                        exel_file["Форма СЭТ-БТ"]
-                        == "Единица измерения: Метрическая тонна"
-                    ].index[0]
-                )
-                + 2
-            )
+            full_exel = pd.read_excel(self.file_path)
+            date_row = full_exel.iloc[2]["Форма СЭТ-БТ"].replace("Дата торгов: ", "")
+            if not date_row:
+                raise ValueError("Не найдена строка с датой торгов")
+            self.date = datetime.strptime(date_row, "%d.%m.%Y")
+            unit_row = full_exel[full_exel["Форма СЭТ-БТ"] == "Единица измерения: Метрическая тонна"].index[0]
+            if not unit_row:
+                raise ValueError("Не найдена нужная таблица")
+            row_number = unit_row + 2
             exel_file = pd.read_excel(
                 self.file_path, usecols="B:F,O", skiprows=row_number
             )
-            exel_file = exel_file[
-                exel_file["Количество\nДоговоров,\nшт."] != "-"
-            ].dropna()
+            if exel_file.empty:
+                raise ValueError("Не найдены данные для обработки")
+            exel_file = exel_file[exel_file["Количество\nДоговоров,\nшт."] != '-'].dropna()
+            if exel_file.empty:
+                raise ValueError("После фильтрации данные отсутствуют")
             logging.info("Данные успешно прочитаны из Excel файла")
             return exel_file
 
         except FileNotFoundError:
             logging.error(f"Файл не найден: {self.file_path}")
+            raise FileNotFoundError
         except pd.errors.EmptyDataError:
             logging.error("Файл пуст")
+            raise pd.errors.EmptyDataError
         except pd.errors.ParserError:
             logging.error("Ошибка парсинга Excel файла")
+            raise pd.errors.ParserError
         except KeyError as e:
             logging.error(f"Отсутствующий столбец в Excel файле: {e}")
+            raise KeyError
         except ValueError as e:
             logging.error(f"Ошибка формата даты: {e}")
+            raise e
+        except IndexError as e:
+            logging.error(f'Таблица не найдена {e}')
+            raise e
         except Exception as e:
             logging.error(f"Неизвестная ошибка при парсинге Excel: {e}")
+            raise e
 
         return None
 
@@ -151,9 +179,15 @@ class SpimexWebParser:
                 self.trade_list.append(trade)
             except ValueError as e:
                 logging.error(f"Ошибка преобразования типов в записи {entry + 1}: {e}")
+                raise e
             except KeyError as e:
                 logging.error(f"Отсутствующий ключ в записи {entry + 1}: {e}")
+                raise e
             except Exception as e:
                 logging.error(f"Неизвестная ошибка в записи {entry + 1}: {e}")
+                raise e
+            except IndexError as e:
+                logging.error(f'Таблица не найдена {e}')
+                raise e
         logging.info(f"Всего обработано {len(self.trade_list)} записей")
         return self.trade_list
